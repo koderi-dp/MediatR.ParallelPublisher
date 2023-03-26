d17f34ffc8484574ffb6217733a8d42baebbb9a2
koderi-dp <peric.danijel@gmail.com>
2023-03-26 13:52:18 +0200

    Update Push.ps1
    skip duplicate

--- Push.ps1
+++ Push.ps1
@@ -8,7 +8,7 @@ if ([string]::IsNullOrEmpty($Env:NUGET_API_KEY)) {
         Write-Host "$($scriptName): Pushing $($_.Name)"
         Write-Host "PACKAGE: $_.FullName"
         
-        dotnet nuget push $_.FullName --source $Env:NUGET_URL --api-key $Env:NUGET_API_KEY
+        dotnet nuget push $_.FullName --source $Env:NUGET_URL --api-key $Env:NUGET_API_KEY --skip-duplicate
         if ($lastexitcode -ne 0) {
             throw ("Exec: " + $errorMessage)
         }
